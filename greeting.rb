fc0e8abacb43acaad1736415e3bcca591a944445
Tanesha Goodman <t24good@icloud.com>
2021-03-13 00:33:30 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,12 @@
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
+end
+
+say_greeting_five_times
---
+++ my_new_directory/README.md
@@ -0,0 +1,2 @@
+my_new_directory $ git remote add origin
+<git@github.com:t24good/good.git>
